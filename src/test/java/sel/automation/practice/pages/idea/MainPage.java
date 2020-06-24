6a5c53d360d164ca62bad6a0c4a1f7e3e7f71071
Andrey7Shulga <ray7sun@gmail.com>
2020-06-24 13:46:12 +0300

    PageHelper class

--- src/test/java/sel/automation/practice/pages/idea/MainPage.java
+++ src/test/java/sel/automation/practice/pages/idea/MainPage.java
@@ -30,7 +30,7 @@ public class MainPage {
 
         menublock.blockToLogin();
 
-        //enter a registered e-mail address as a login
+        //enter the registered e-mail address as a login
 //        $("#email").clear();
 //        $("#email").setValue(login);
         ph.setValueToCssElement("#email", login);
