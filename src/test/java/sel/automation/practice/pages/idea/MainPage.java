2d72baff6ef6c9ba85c3f93beb16d629d86e265c
Andrey7Shulga <ray7sun@gmail.com>
2020-06-24 15:35:33 +0300

    pushing

--- src/test/java/sel/automation/practice/pages/idea/MainPage.java
+++ src/test/java/sel/automation/practice/pages/idea/MainPage.java
@@ -30,7 +30,7 @@ public class MainPage {
 
         menublock.blockToLogin();
 
-        //Enter a registered e-mail address as a login
+        //enter a registered e-mail address as a login
 //        $("#email").clear();
 //        $("#email").setValue(login);
         ph.setValueToCssElement("#email", login);
